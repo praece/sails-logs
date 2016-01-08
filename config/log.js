ffdb618f5b286bd37235e00fafe722382e9cc843
Kevin OBrien <kevin.obrien@praece.com>
2016-01-08 13:21:09 -0800

    Initial commit.

---
+++ config/log.js
@@ -0,0 +1,75 @@
+/**
+ * Built-in Log Configuration
+ * (sails.config.log)
+ *
+ * Configure the log level for your app, as well as the transport
+ * (Underneath the covers, Sails uses Winston for logging, which
+ * allows for some pretty neat custom transports/adapters for log messages)
+ *
+ * For more information on the Sails logger, check out:
+ * http://sailsjs.org/#!/documentation/concepts/Logging
+ */
+
+var winston = require('winston');
+var config  = require('winston/lib/winston/config');
+var _       = require('lodash');
+
+var customLogger = new winston.Logger({
+  rewriters: [function (level, msg, meta) {
+    if (meta.error) meta.stack = meta.error.stack.split('\n');
+
+    return meta;
+  }]
+});
+
+customLogger.add(winston.transports.Console, {
+  level: 'debug',
+  colorize: true,
+  formatter: function (options) {
+    var output = '';
+    output += config.colorize(options.level, options.level + ': ');
+
+    if (options.meta.error) {
+      output += options.meta.error.stack;
+      delete options.meta.error;
+      delete options.meta.stack;
+    } else {
+      output += options.message;
+    }
+
+    if (!_.isEmpty(options.meta)) {
+      output += '\n';
+      output += JSON.stringify(options.meta, null, 2);
+    }
+
+    return output;
+  }
+});
+
+customLogger.add(winston.transports.File, {
+  level: 'error',
+  maxsize: 10*1024*1024,
+  maxFiles: 4,
+  tailable: true,
+  filename: 'app.log'
+});
+
+module.exports.log = {
+
+  /***************************************************************************
+  *                                                                          *
+  * Valid `level` configs: i.e. the minimum log level to capture with        *
+  * sails.log.*()                                                            *
+  *                                                                          *
+  * The order of precedence for log levels from lowest to highest is:        *
+  * silly, verbose, info, debug, warn, error                                 *
+  *                                                                          *
+  * You may also set the level to "silent" to suppress all logs.             *
+  *                                                                          *
+  ***************************************************************************/
+
+  custom: customLogger,
+  level: 'silly',
+  inspect: false
+
+};
---
+++ index.js
@@ -0,0 +1,21 @@
+var Mail = require('./lib/email').Mail;
+var _    = require('lodash');
+
+module.exports = function (sails) {
+  var loader     = require('sails-util-mvcsloader')(sails);
+
+  // Load config from default directories
+  loader.configure({
+    config: __dirname + '/config' // Path to the config to load
+  });
+
+  return {
+  	configure: function () {
+      if (sails.config.log.email) {
+        var config = _.assign({level: 'error', json: true}, sails.config.log.email)
+
+        sails.config.log.custom.add(Mail, config);
+      }
+  	}
+  };
+};
